0e35827a578215849611b623f4d7359d30047ca2
Michael Nenninger <m-nenninger@swissphone-systems.de>
2016-12-07 08:16:33 +0100

    first batchfiles for building

---
+++ windows/build_protobuf_vs_2015.cmd
@@ -0,0 +1,76 @@
+@echo off
+SETLOCAL ENABLEEXTENSIONS
+echo Build protbuf with VS2015 on windows
+
+set basedir=%1
+
+if "%basedir%"=="" (
+	goto ExitError
+)
+
+cd %basedir%
+
+IF NOT EXIST "protobuf-cpp-3.1.0.tar.gz" (
+	wget https://github.com/google/protobuf/releases/download/v3.1.0/protobuf-cpp-3.1.0.tar.gz -O protobuf-cpp-3.1.0.tar.gz >> build_cmd.log
+	IF %ERRORLEVEL% NEQ 0 (
+	  echo Error extract zlib
+	  exit /b %ERRORLEVEL%
+	)
+	7z x protobuf-cpp-3.1.0.tar.gz -so | 7z x -aoa -si -ttar -o"protobuf-cpp"
+	rem 7z x protobuf-cpp-3.1.0.tar.gz -oprotobuf-cpp -y >> build_cmd.log
+	
+	cd protobuf-cpp
+	cd protobuf-3.1.0
+	echo copy files to root build dir
+	
+	xcopy /R /F /Y /E /S *.* ..
+	
+	cd ..
+	rmdir /S /Q protobuf-3.1.0
+	
+	REM in release this allready included
+	REM git clone -b release-1.7.0 https://github.com/google/googlemock.git gmock
+	REM cd gmock
+	REM git clone -b release-1.7.0 https://github.com/google/googletest.git gtest
+	
+	cd cmake
+	mkdir build 
+	cd build
+	mkdir release
+	cd release
+	
+	echo build protobuf release
+	
+	if "%PLATFORM%"=="x64" (
+		cmake -G "Visual Studio 14 2015 Win64" -DCMAKE_INSTALL_PREFIX=../../../../protobuf ../.. >> ../../../../build_cmd.log
+	) else (
+		cmake -G "Visual Studio 14 2015" -DCMAKE_INSTALL_PREFIX=../../../../protobuf ../.. >> ../../../../build_cmd.log
+	)
+    
+	echo cmake result %ERRORLEVEL%
+	
+	IF %ERRORLEVEL% NEQ 0 (
+	  echo Error cmake build files protobuf
+	  exit /b %ERRORLEVEL%
+	)
+	
+	echo compiling
+	
+	cmake --build . --target install --config Release >> ../../../../build_cmd.log
+	
+	REM nmake check >> build_cmd.log
+	
+	REM echo build protobuf debug
+	
+	REM cd ..
+	REM mkdir debug
+	REM cd debug
+	REM cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=../../../../install ../.. >> build_cmd.log
+	REM cmake --build . >> build_cmd.log
+)
+
+
+:ExitError
+echo build Failed
+echo Check LIBOSMBASEDIR is set
+echo lock at %basedir%\build_cmd.log
---
+++ windows/build_zlib_vs_2015.cmd
@@ -0,0 +1,89 @@
+@echo off
+SETLOCAL ENABLEEXTENSIONS
+echo Build zlib with VS2015 on windows
+
+REM https://www.cairographics.org/end_to_end_build_for_win32/
+
+set basedir=%1
+
+if "%basedir%"=="" (
+	goto ExitError
+)
+
+cd %basedir%
+
+IF NOT EXIST "zlib.tar.gz" (
+   echo Downloading ZLIB source
+   wget -o build_cmd.log -O zlib.tar.gz http://www.zlib.net/zlib-1.2.8.tar.gz
+   IF %ERRORLEVEL% NEQ 0 (
+	  echo Error Downloading ZLIB 
+	  exit /b %ERRORLEVEL%
+   )
+   7z x zlib.tar.gz -so | 7z x -aoa -si -ttar -o"zlib"
+   IF %ERRORLEVEL% NEQ 0 (
+	  echo Error unzip ZLIB 
+	  exit /b %ERRORLEVEL%
+   )
+)
+
+cd zlib
+if NOT EXIST "bin" (
+   mkdir bin
+)
+
+if NOT EXIST "lib" (
+   mkdir lib
+)
+
+if NOT EXIST "include" (
+   mkdir include
+)
+
+cd zlib-1.2.8\contrib\masmx86
+ml /coff /Zi /c /safeseh /Flmatch686.lst match686.asm
+ml /coff /Zi /c /safeseh /Flinffas32.lst inffas32.asm
+cd ..\vstudio
+if NOT EXIST "vc14" (
+	mkdir vc14
+	cd vc11
+    xcopy /R /F /Y /E /S *.* ..\vc14
+	cd ..\vc14
+	devenv.exe /Upgrade zlibvc.sln
+	cd ..
+)
+cd vc14
+if NOT EXIST "UpgradeLog.htm" (
+   devenv.exe /Upgrade zlibvc.sln 
+)
+
+REM Todo .def remove version. find better way
+xcopy /F /R /Y %basedir%\..\windows\zlibvc.def .
+
+call msbuild zlibvc.sln /p:Configuration=Release /p:Platform=Win32 /p:PreBuildEventUseInBuild=false
+IF %ERRORLEVEL% NEQ 0 (
+  echo Error building release zlib
+  exit /b %ERRORLEVEL%
+)
+
+call msbuild zlibvc.sln /p:Configuration=Debug /p:Platform=Win32 /p:PreBuildEventUseInBuild=false
+IF %ERRORLEVEL% NEQ 0 (
+  echo Error building debug zlib
+  exit /b %ERRORLEVEL%
+)
+
+REM rename and move so cmake can find it
+copy /b /y x86\ZlibDllRelease\zlibwapi.dll %basedir%\zlib\bin\zlib1.dll
+copy /b /y x86\ZlibDllRelease\zlibwapi.pdb %basedir%\zlib\bin\zlib1.pdb
+copy /b /y x86\ZlibDllRelease\zlibwapi.lib %basedir%\zlib\lib\zlib.lib
+copy /b /y %basedir%\zlib\zlib-1.2.8\zconf.h %basedir%\zlib\include\zconf.h
+copy /b /y %basedir%\zlib\zlib-1.2.8\zlib.h %basedir%\zlib\include\zlib.h
+
+goto noerror
+
+:ExitError
+echo build Failed
+echo Check LIBOSMBASEDIR is set
+echo lock at %basedir%\build_cmd.log
+
+:noerror
+echo zlib build
