bc4d49bf955cfa608e7455a65a94b6d8e88e6048
Michael Nenninger <m-nenninger@swissphone-systems.de>
2017-06-09 07:31:04 +0200

    add qt hints to cmake

--- windows/build_cmake_vs2015.cmd
+++ windows/build_cmake_vs2015.cmd
@@ -18,6 +18,7 @@ cd %BUILDDIR%
 set CMAKE_INCLUDE_PATH=%BUILDDIR%\libiconv\include;%BUILDDIR%\libxml2\include;%BUILDDIR%\protobuf\include;%BUILDDIR%\zlib\include;%BUILDDIR%\cairo-build\include;D:\Tools\Qt\5.8\msvc2015\include
 set CMAKE_LIBRARY_PATH=%BUILDDIR%\libxml2\lib;%BUILDDIR%\protobuf\lib;%BUILDDIR%\zlib\lib;%BUILDDIR%\cairo-build\lib;D:\Tools\Qt\5.8\msvc2015\lib
 SET CMAKE_PROGRAM_PATH=%BUILDDIR%\protobuf\bin
+SET CMAKE_PREFIX_PATH=D:\Tools\Qt\5.8\msvc2015
 
-cmake -G "Visual Studio 14 2015" -DCMAKE_SYSTEM_VERSION=10.0.##### ..
-cmake --build .
+cmake -G "Visual Studio 14 2015" -DCMAKE_SYSTEM_VERSION=10.0.##### .. -DCMAKE_INSTALL_PREFIX=.\output -DOSMSCOUT_BUILD_IMPORT=OFF -DOSMSCOUT_BUILD_DOC_API=OFF -DOSMSCOUT_BUILD_TESTS=OFF -DOSMSCOUT_BUILD_MAP_CAIRO=OFF
+cmake --build . --target install
